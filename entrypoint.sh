9daaed5310b412437a8960c5f7f41a00136c4560
Ids van der Molen <2285220+idsvandermolen@users.noreply.github.com>
2026-05-09 16:56:22 +0200

    Save container entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -euo pipefail
+uv run ./manage.py check
+uv run ./manage.py collectstatic --noinput
+uv run ./manage.py migrate --noinput
+# uv run manage.py shell -c 'from api.load import run;run()'
+uv run ./manage.py runserver 0.0.0.0:8000
