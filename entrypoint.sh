ed12866bc6aed0654524f644770f5ebb72ef6785
Ids van der Molen <2285220+idsvandermolen@users.noreply.github.com>
2026-05-13 15:56:20 +0200

    Use gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ uv run ./manage.py check
 uv run ./manage.py collectstatic --noinput
 uv run ./manage.py migrate --noinput
 uv run manage.py shell -c 'from api.load import run;run()'
-uv run ./manage.py runserver 0.0.0.0:8000
+uv run gunicorn --config etc/gunicorn.conf.py
